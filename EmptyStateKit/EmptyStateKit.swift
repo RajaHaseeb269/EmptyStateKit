57a44a3581def68ac1c6e714027eb465ce05ba00
Muhammad Haseeb ur Rehman <haseeb@Muhammads-MacBook-Pro.local>
2025-09-02 19:05:18 +0200

    Initial commit

---
+++ EmptyStateKit/EmptyStateKit.swift
@@ -0,0 +1,9 @@
+//
+//  EmptyStateKit.swift
+//  EmptyStateKit
+//
+//  Created by Muhammad Haseeb ur Rehman on 02.09.25.
+//
+
+import Foundation
+
---
+++ EmptyStateKitTests/EmptyStateKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  EmptyStateKitTests.swift
+//  EmptyStateKitTests
+//
+//  Created by Muhammad Haseeb ur Rehman on 02.09.25.
+//
+
+import Testing
+@testable import EmptyStateKit
+
+struct EmptyStateKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
